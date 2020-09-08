8242f57538410df9f80849f0a1422c50728cd3ee
JepoyOrias <jeffreyorais26@gmail.com>
2020-09-08 14:10:13 +0800

    connect vue routes to laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
 require('./bootstrap');
+window.Vue = require("vue");
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+let routes = [
+    {path: '/dashboard', component: require('./components/Dashboard.vue').default},
+    {path: '/profile' , component: require('./components/Profile.vue').default}
+
+]
+
+const router = new VueRouter({
+    routes
+});
+
+
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+const app = new Vue({
+    el: '#app',
+    router
+});
